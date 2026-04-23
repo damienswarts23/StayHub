ff1db5571aad1697694d63f2274fae76469cadd8
Damien Swarts <222868791@mycput.ac.za>
2026-04-23 15:47:57 +0200

    adding this file here so we can connect to the database succesfully to read, write, update and delete

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$dbname = "cput_stays";
+$username = "root";
+$password = "";
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
+
+if ($mysqli->connect_errno) {
+    die("Connection error: " . $mysqli->connect_error);
+}
+
+$mysqli->set_charset("utf8mb4");
+
+return $mysqli;
